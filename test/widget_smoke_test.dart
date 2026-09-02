ab085f69b68f3ba41c8e7195e0cec976600eb6c2
md-kks <110589018+md-kks@users.noreply.github.com>
2026-09-02 19:06:07 +0200

    Add widget smoke test

---
+++ test/widget_smoke_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rechenblitz/main.dart';
+import 'package:rechenblitz/services/app_controller.dart';
+
+void main() {
+  testWidgets('Startseite zeigt zentrale Lernmodi', (tester) async {
+    final controller = AppController();
+    controller.facts = const [];
+    controller.loaded = true;
+    await tester.pumpWidget(RechenblitzApp(controller: controller));
+    expect(find.text('Hallo!'), findsOneWidget);
+    expect(find.text('Üben'), findsOneWidget);
+    expect(find.text('Minus üben'), findsOneWidget);
+    expect(find.text('Tempotest'), findsOneWidget);
+  });
+}
