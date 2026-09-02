725af6fa1600bf38deebe2cb150508f77c5f9d7d
md-kks <110589018+md-kks@users.noreply.github.com>
2026-09-02 19:08:58 +0200

    Fix analyzer warning in widget test

--- test/widget_smoke_test.dart
+++ test/widget_smoke_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rechenblitz/main.dart';
 import 'package:rechenblitz/services/app_controller.dart';
